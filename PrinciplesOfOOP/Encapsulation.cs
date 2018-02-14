6dfa9b0b5759f83cfe8a577a6858690a26e7e98e
Shawn Hill <shawn.hill121@gmail.com>
2018-02-14 12:11:44 -0500

    initial commit

---
+++ PrinciplesOfOOP/Encapsulation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrinciplesOfOOP
+{
+    public class Encapsulation
+    {
+        private static string iAmPrivate = "I am private. You may not access me.";
+
+        public static string iAmPublic = "I am public. You may access me.";
+
+        public static string iAmReturned()
+        {
+            return iAmPrivate;
+        }
+    }
+}
---
+++ PrinciplesOfOOP/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PrinciplesOfOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Encapsulation.iAmPublic); //This string is accessable, because it is public.
+
+            // Console.WriteLine(Encapsulation.iAmPrivate); This string is inaccessable, because it is private. 
+
+            Console.WriteLine(Encapsulation.iAmReturned()); //The string 'iAmPrivate' is accessed internally by 'iAmReturned' which returns the value of 'iAmPrivate'
+
+        }
+    }
+}
